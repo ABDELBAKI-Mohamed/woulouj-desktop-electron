2185f30328bf96553253bf37461a48d48e0f562c
ABDELBAKI-Mohamed <abdelbaki.mo6@gmail.com>
2023-05-30 02:02:20 +0100

    the whole output folder..

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,672 @@
+const client_manifest = {
+  "_FilterGroups.vue.499daa25.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "FilterGroups.vue.499daa25.js",
+    "imports": [
+      "_UiSelect.vue.78363163.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useLanguagesStore.4ee7b7fd.js"
+    ]
+  },
+  "_ImageUploader.vue.71b3de40.js": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "inbox.d3c7c522.svg"
+    ],
+    "file": "ImageUploader.vue.71b3de40.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "inbox.d3c7c522.svg": {
+    "file": "inbox.d3c7c522.svg",
+    "resourceType": "image",
+    "mimeType": "image/svg+xml"
+  },
+  "_UiButton.vue.e4d74fdb.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "UiButton.vue.e4d74fdb.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_UiCard.vue.de9e9fa0.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "UiCard.vue.de9e9fa0.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_UiInput.vue.f82dde06.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "UiInput.vue.f82dde06.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_UiSelect.vue.78363163.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "UiSelect.vue.78363163.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_UiTable.vue.109b2e7c.js": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "trash.58097607.svg",
+      "eye.9380e5f7.svg",
+      "kebab.5c7a363f.svg"
+    ],
+    "file": "UiTable.vue.109b2e7c.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_getRightPath.3ee11a98.js"
+    ]
+  },
+  "trash.58097607.svg": {
+    "file": "trash.58097607.svg",
+    "resourceType": "image",
+    "mimeType": "image/svg+xml"
+  },
+  "eye.9380e5f7.svg": {
+    "file": "eye.9380e5f7.svg",
+    "resourceType": "image",
+    "mimeType": "image/svg+xml"
+  },
+  "kebab.5c7a363f.svg": {
+    "file": "kebab.5c7a363f.svg",
+    "resourceType": "image",
+    "mimeType": "image/svg+xml"
+  },
+  "_getRightPath.3ee11a98.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "getRightPath.3ee11a98.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_nuxt-link.cdc55fd2.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "nuxt-link.cdc55fd2.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_useAuth.0a9685b0.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "useAuth.0a9685b0.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_useGlobalState.05eb19db.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "useGlobalState.05eb19db.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_useLanguagesStore.4ee7b7fd.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "useLanguagesStore.4ee7b7fd.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "_useToast.0d39c995.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "useToast.0d39c995.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/Icons/affiliation.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "affiliation.59883a5f.png",
+    "src": "assets/Icons/affiliation.png"
+  },
+  "assets/Icons/assistant.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "assistant.d55bacad.png",
+    "src": "assets/Icons/assistant.png"
+  },
+  "assets/Icons/groups.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "groups.0239ccdf.png",
+    "src": "assets/Icons/groups.png"
+  },
+  "assets/Icons/salary.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "salary.58df766f.png",
+    "src": "assets/Icons/salary.png"
+  },
+  "assets/Icons/student.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "student.8edde172.png",
+    "src": "assets/Icons/student.png"
+  },
+  "assets/Icons/teacher.png": {
+    "resourceType": "image",
+    "mimeType": "image/png",
+    "file": "teacher.679a5f6a.png",
+    "src": "assets/Icons/teacher.png"
+  },
+  "assets/svgs/arrow-up.svg": {
+    "resourceType": "image",
+    "mimeType": "image/svg+xml",
+    "file": "arrow-up.26e35ba2.svg",
+    "src": "assets/svgs/arrow-up.svg"
+  },
+  "assets/svgs/eye.svg": {
+    "resourceType": "image",
+    "mimeType": "image/svg+xml",
+    "file": "eye.9380e5f7.svg",
+    "src": "assets/svgs/eye.svg"
+  },
+  "assets/svgs/inbox.svg": {
+    "resourceType": "image",
+    "mimeType": "image/svg+xml",
+    "file": "inbox.d3c7c522.svg",
+    "src": "assets/svgs/inbox.svg"
+  },
+  "assets/svgs/kebab.svg": {
+    "resourceType": "image",
+    "mimeType": "image/svg+xml",
+    "file": "kebab.5c7a363f.svg",
+    "src": "assets/svgs/kebab.svg"
+  },
+  "assets/svgs/trash.svg": {
+    "resourceType": "image",
+    "mimeType": "image/svg+xml",
+    "file": "trash.58097607.svg",
+    "src": "assets/svgs/trash.svg"
+  },
+  "components/CreateAffiliation.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "CreateAffiliation.f7dbe26b.js",
+    "imports": [
+      "_UiSelect.vue.78363163.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiInput.vue.f82dde06.js",
+      "_FilterGroups.vue.499daa25.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_useToast.0d39c995.js",
+      "_useLanguagesStore.4ee7b7fd.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/CreateAffiliation.vue"
+  },
+  "components/CreateAssistant.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "CreateAssistant.6723b934.js",
+    "imports": [
+      "_ImageUploader.vue.71b3de40.js",
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_useGlobalState.05eb19db.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useToast.0d39c995.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/CreateAssistant.vue"
+  },
+  "components/CreateGroup.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "CreateGroup.aec96862.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiSelect.vue.78363163.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useLanguagesStore.4ee7b7fd.js",
+      "_useGlobalState.05eb19db.js",
+      "_useToast.0d39c995.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/CreateGroup.vue"
+  },
+  "components/CreateSalary.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "CreateSalary.f4195a6a.js",
+    "imports": [
+      "_UiSelect.vue.78363163.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiInput.vue.f82dde06.js",
+      "_FilterGroups.vue.499daa25.js",
+      "_useGlobalState.05eb19db.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useToast.0d39c995.js",
+      "_useLanguagesStore.4ee7b7fd.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/CreateSalary.vue"
+  },
+  "components/CreateStudent.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "CreateStudent.c6315916.js",
+    "imports": [
+      "_ImageUploader.vue.71b3de40.js",
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_useGlobalState.05eb19db.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useToast.0d39c995.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/CreateStudent.vue"
+  },
+  "components/CreateTeacher.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "CreateTeacher.8c370c89.js",
+    "imports": [
+      "_ImageUploader.vue.71b3de40.js",
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_useGlobalState.05eb19db.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useToast.0d39c995.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/CreateTeacher.vue"
+  },
+  "components/Delete.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "Delete.5869f6dc.js",
+    "imports": [
+      "_UiButton.vue.e4d74fdb.js",
+      "_useGlobalState.05eb19db.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "components/Delete.vue"
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "arrow-up.26e35ba2.svg"
+    ],
+    "dynamicImports": [
+      "components/CreateGroup.vue",
+      "components/CreateSalary.vue",
+      "components/CreateAffiliation.vue",
+      "components/CreateTeacher.vue",
+      "components/CreateStudent.vue",
+      "components/CreateAssistant.vue",
+      "components/Delete.vue"
+    ],
+    "file": "default.89694c21.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_useToast.0d39c995.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "layouts/default.vue"
+  },
+  "arrow-up.26e35ba2.svg": {
+    "file": "arrow-up.26e35ba2.svg",
+    "resourceType": "image",
+    "mimeType": "image/svg+xml"
+  },
+  "node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.e33ab215.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "css": [
+      "entry.e33ab215.css"
+    ],
+    "dynamicImports": [
+      "layouts/default.vue",
+      "virtual:nuxt:C:/Users/abdel/OneDrive/Desktop/Projects/tauri/ibn-mandoor/.nuxt/error-component.mjs"
+    ],
+    "file": "entry.1b4632b8.js",
+    "isEntry": true,
+    "src": "node_modules/nuxt/dist/app/entry.js"
+  },
+  "entry.e33ab215.css": {
+    "file": "entry.e33ab215.css",
+    "resourceType": "style"
+  },
+  "pages/assistants/[id].vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "_id_.6825dd48.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/assistants/[id].vue"
+  },
+  "pages/assistants/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.ab007f25.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiTable.vue.109b2e7c.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/assistants/index.vue"
+  },
+  "pages/auth/login.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "login.375971d7.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_useAuth.0a9685b0.js",
+      "_useToast.0d39c995.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/auth/login.vue"
+  },
+  "pages/auth/register.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "register.792edb39.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_useAuth.0a9685b0.js",
+      "_useToast.0d39c995.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/auth/register.vue"
+  },
+  "pages/groups/[id].vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "_id_.99057ac5.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/groups/[id].vue"
+  },
+  "pages/groups/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.b363d6dc.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiSelect.vue.78363163.js",
+      "_useLanguagesStore.4ee7b7fd.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiTable.vue.109b2e7c.js",
+      "_useGlobalState.05eb19db.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/groups/index.vue"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "student.8edde172.png",
+      "teacher.679a5f6a.png",
+      "assistant.d55bacad.png",
+      "affiliation.59883a5f.png",
+      "salary.58df766f.png",
+      "groups.0239ccdf.png"
+    ],
+    "file": "index.b5a6c453.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  },
+  "student.8edde172.png": {
+    "file": "student.8edde172.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "teacher.679a5f6a.png": {
+    "file": "teacher.679a5f6a.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "assistant.d55bacad.png": {
+    "file": "assistant.d55bacad.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "affiliation.59883a5f.png": {
+    "file": "affiliation.59883a5f.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "salary.58df766f.png": {
+    "file": "salary.58df766f.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "groups.0239ccdf.png": {
+    "file": "groups.0239ccdf.png",
+    "resourceType": "image",
+    "mimeType": "image/png"
+  },
+  "pages/languages/[language]/[level]/[module]/[age]/[group]/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.9d1f7e1d.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/languages/[language]/[level]/[module]/[age]/[group]/index.vue"
+  },
+  "pages/languages/[language]/[level]/[module]/[age]/groups.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "groups.4abddc11.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/languages/[language]/[level]/[module]/[age]/groups.vue"
+  },
+  "pages/languages/[language]/[level]/[module]/ages.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "ages.b6e241a1.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "_useLanguagesStore.4ee7b7fd.js",
+      "_getRightPath.3ee11a98.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/languages/[language]/[level]/[module]/ages.vue"
+  },
+  "pages/languages/[language]/[level]/modules.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "modules.77f4f32e.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useLanguagesStore.4ee7b7fd.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/languages/[language]/[level]/modules.vue"
+  },
+  "pages/languages/[language]/levels.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "levels.90b7849e.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useLanguagesStore.4ee7b7fd.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/languages/[language]/levels.vue"
+  },
+  "pages/languages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.4543905e.js",
+    "imports": [
+      "_nuxt-link.cdc55fd2.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/languages/index.vue"
+  },
+  "pages/payment/affiliation/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.ae6ca3cd.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiTable.vue.109b2e7c.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/payment/affiliation/index.vue"
+  },
+  "pages/payment/salary/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.38a80b5d.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiTable.vue.109b2e7c.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/payment/salary/index.vue"
+  },
+  "pages/students/[id].vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "_id_.8cbe0deb.js",
+    "imports": [
+      "_UiCard.vue.de9e9fa0.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/students/[id].vue"
+  },
+  "pages/students/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.933bc1a0.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiTable.vue.109b2e7c.js",
+      "_useGlobalState.05eb19db.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/students/index.vue"
+  },
+  "pages/teachers/[id].vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "_id_.f2973b75.js",
+    "imports": [
+      "_UiCard.vue.de9e9fa0.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/teachers/[id].vue"
+  },
+  "pages/teachers/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "index.27692dae.js",
+    "imports": [
+      "_UiInput.vue.f82dde06.js",
+      "_UiButton.vue.e4d74fdb.js",
+      "_UiTable.vue.109b2e7c.js",
+      "node_modules/nuxt/dist/app/entry.js",
+      "_useGlobalState.05eb19db.js",
+      "_nuxt-link.cdc55fd2.js",
+      "_getRightPath.3ee11a98.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/teachers/index.vue"
+  },
+  "virtual:nuxt:C:/Users/abdel/OneDrive/Desktop/Projects/tauri/ibn-mandoor/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-component.c8c3b4d8.js",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "virtual:nuxt:C:/Users/abdel/OneDrive/Desktop/Projects/tauri/ibn-mandoor/.nuxt/error-component.mjs"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
